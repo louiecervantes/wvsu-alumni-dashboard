ca085bf167f638c7e1fcab5c39441bc93800246d
unknown <louiecervantes@wvsu.edu.ph>
2023-04-01 03:53:28 +0800

    save changes

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,2 @@
+# WVSU Alumni Dashboard
+A comprehensive management dashboard that provides insights into the university's alumni network and helps the institution make data-driven decisions
---
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -0,0 +1,43 @@
+#Input the relevant libraries
+import numpy as np
+import pandas as pd
+import streamlit as st
+import altair as alt
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+from scipy.stats import chi2_contingency
+import scipy.stats as stats
+
+
+# Define the Streamlit app
+def app():
+    # Load image from file
+    img = Image.open("example_image.jpg")
+    img = img.resize(200,200)
+    st.image(img, width=200, caption="Example Image")
+
+    st.title("Welcome to the WVSU Alumni Dashboard")
+    st.subheader("(c) 2023 WVSU Management Information System")
+                 
+    st.write("This tool is managed by:")
+    st.write("Dr. Jonathan P. Glorial")
+    st.write("Director")
+    st.write("WVSU Alumni Office")
+    st.write("alumni@wvsu.edu.ph")
+                 
+    st.write("A university alumni dashboard is a visual representation of data that provides insights into the institution's alumni network. It typically includes key performance indicators such as demographics, employment status, industry and job titles, alumni engagement, student success, alumni feedback, social media engagement, giving history, and contact information. The dashboard is designed to provide a quick and easy way for university administrators to monitor and analyze data related to their alumni network, identify trends, and make data-driven decisions to improve alumni relations and engagement.")
+
+    st.subheader("Demographics")
+    df = pd.read_csv('2013-main.csv', dtype='str', header=0, 
+        sep = ",", encoding='latin')
+    st.dataframe(df, width=800, height=400)
+    st.write("Properties of the dataset")
+    desc = df.describe().T
+    st.write(desc)
+
+    if st.button('Begin'):
+ 
+# Run the app
+if __name__ == "__main__":
+    app()
